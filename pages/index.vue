748ce23d70f640aa78f91b00530ae7da6514377f
Hasan Mahmud <hasansujon786@gmail.com>
2019-11-05 23:00:05 +0600

    update pag file

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,16 @@
     <div>
       <logo />
       <h1 class="title">
-        nuxanger
+        nuxenger
       </h1>
       <h2 class="subtitle">
         My tiptop Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
           GitHub
         </a>
       </div>
@@ -54,8 +46,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
